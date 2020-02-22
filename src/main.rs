fd9ffd35533e6e591a269d59c64810975fd33036
Keitaro Oguri <ogukei256@gmail.com>
2020-02-23 03:01:55 +0900

    Update README.md

--- src/main.rs
+++ src/main.rs
@@ -9,31 +9,36 @@ fn main() {
 }
 
 fn fibonacci(num_elements: usize) {
+    // setup data to calculate fibonacci sequence
+    let mut v: Vec<u32> = (0..num_elements as u32).collect();
+    // filum automatically selects one from available GPUs. 
+    // context contains information of the GPU.
     let context = Context::new().unwrap();
-    // layout memory
-    let view = BufferViewBuilder::new(&context)
+    // allocates contiguous memory of u32 with the number of `num_elements`.
+    let buffer_view = BufferViewBuilder::new(&context)
         .bind_array::<u32>(num_elements)
         .build()
         .unwrap();
-    // prepare compute shader
-    let pipeline = PipelineBuilder::new(view.buffer())
+    // loads a compute shader from the specified file path
+    // and associates it to the buffer.
+    let pipeline = PipelineBuilder::new(buffer_view.buffer())
         .shader("data/fibonacci.comp.spv")
-        .specialization(constants!(num_elements as u32))
         .build()
         .unwrap();
-    // send data to GPU device
-    let mut v: Vec<u32> = (0..num_elements as u32)
-        .collect();
-    let binding = view.binding();
+    // in order to transfer our data to the GPU, 
+    // gets a reference which corresponds to the binding point that
+    // indicates the location of the array of u32 stored on the GPU.
+    let binding = buffer_view.binding();
+    // sends data to the GPU
     binding.update_array_copying(&v);
-    // execute computation
+    // runs the computation specifying how many invocations of 
+    // the shader performed.
     pipeline.dispatch(num_elements);
-    // receive data from GPU device
+    // retrieves back data from the GPU
     binding.fetch_array_copying(&mut v);
     println!("{:?}", v);
 }
 
-// an implementation of
 // A Parallel Approach to Object Identification in Large-scale Images
 // @see https://www.academia.edu/29842500/
 fn connected_component_labeling() {
